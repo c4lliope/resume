a965278f50ebf1f02360c8737f716a3a1a575f28
Grayson Wright <wright.grayson@gmail.com>
2014-08-12 23:02:53 -0400

    Add data store to Education::Topic

--- content/education.rb
+++ content/education.rb
@@ -1,33 +1,52 @@
-class EducationTopic
-  class InvalidRecord < StandardError; end
+module Education
+  class Topic
+    def self.store
+      @@store ||= []
+    end
 
-  def initialize name, gpa
-    @name, @gpa = name, gpa
+    def self.all
+      store
+    end
 
-    raise InvalidRecord unless valid?
-  end
+    def save
+      Topic.store << self
+      self
+    end
 
-  attr_reader :name
+    def self.destroy_all
+      @@store = nil
+    end
 
-  def gpa
-    @gpa.to_f
-  end
+    class InvalidRecord < StandardError; end
 
-  def formatted_gpa
-    "%.1f" % gpa
-  end
+    def initialize name, gpa
+      @name, @gpa = name, gpa
 
-  def valid?
-    valid_name? and valid_gpa?
-  end
+      raise InvalidRecord unless valid?
+    end
 
-  private
+    attr_reader :name
 
-  def valid_name?
-    name
-  end
+    def gpa
+      @gpa.to_f
+    end
 
-  def valid_gpa?
-    @gpa && gpa >= 0 && gpa <= 4
+    def formatted_gpa
+      "%.1f" % gpa
+    end
+
+    def valid?
+      valid_name? and valid_gpa?
+    end
+
+    private
+
+    def valid_name?
+      name
+    end
+
+    def valid_gpa?
+      @gpa && gpa >= 0 && gpa <= 4
+    end
   end
 end
--- test/education_test.rb
+++ test/education_test.rb
@@ -3,36 +3,44 @@ require_relative '../content/education'
 
 class EducationTopicTest < Minitest::Test
   def test_valid_record
-    topic = EducationTopic.new 'math', 4.0
+    topic = Education::Topic.new 'math', 4.0
     assert_equal true, topic.valid?
   end
 
   def test_topic_must_have_name
-    assert_raises(EducationTopic::InvalidRecord) do
-      EducationTopic.new nil, 4.0
+    assert_raises(Education::Topic::InvalidRecord) do
+      Education::Topic.new nil, 4.0
     end
   end
 
   def test_topic_must_have_gpa
-    assert_raises(EducationTopic::InvalidRecord) do
-      EducationTopic.new 'math', nil
+    assert_raises(Education::Topic::InvalidRecord) do
+      Education::Topic.new 'math', nil
     end
   end
 
   def test_gpa_is_at_most_4
-    assert_raises(EducationTopic::InvalidRecord) do
-      EducationTopic.new 'math', 4.1
+    assert_raises(Education::Topic::InvalidRecord) do
+      Education::Topic.new 'math', 4.1
     end
   end
 
   def test_gpa_is_at_least_0
-    assert_raises(EducationTopic::InvalidRecord) do
-      EducationTopic.new 'math', -0.1
+    assert_raises(Education::Topic::InvalidRecord) do
+      Education::Topic.new 'math', -0.1
     end
   end
 
   def test_gpa_is_displayed_to_one_digit
-    topic = EducationTopic.new 'math', '3.85'
+    topic = Education::Topic.new 'math', '3.85'
     assert_equal '3.9', topic.formatted_gpa
+  end
+
+  def test_topic_storage
+    topic = Education::Topic.new 'math', '3.85'
+    topic.save
+    assert_equal [topic], Education::Topic.all
+    Education::Topic.destroy_all
+    assert_equal [], Education::Topic.all
   end
 end
