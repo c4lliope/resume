262b4ea22a8f570f1708de10ae8e29b60b4bea1c
Grayson Wright <wright.grayson@gmail.com>
2016-08-15 21:31:59 -0700

    Use smart quotes in our markdown
    See:
    https://github.com/vmg/redcarpet#also-now-our-pants-are-much-smarter
    http://stackoverflow.com/questions/11781354/how-can-i-do-github-style-markdown-rendering-from-sinatra

--- server.rb
+++ server.rb
@@ -1,6 +1,14 @@
 require "sinatra"
 require "sinatra/reloader"
 require "yaml"
+require "redcarpet"
+
+class HTMLWithPants < Redcarpet::Render::HTML
+  include Redcarpet::Render::SmartyPants
+end
+
+Tilt.register Tilt::RedcarpetTemplate::Redcarpet2, 'markdown', 'mkd', 'md'
+set :markdown, renderer: HTMLWithPants
 
 get "/" do
   @data = YAML.load_file "content.yml"
